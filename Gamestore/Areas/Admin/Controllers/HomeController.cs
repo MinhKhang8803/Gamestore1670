3110015878df9c796569af808fb0be89719561f9
Minh Khang <khangbmgch210522@fpt.edu.vn>
2023-06-21 13:51:08 +0700

    Add Admin Function

---
+++ Gamestore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gamestore.Areas.Admin.Models;
+using Gamestore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gamestore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly GamestoreContext _context;
+
+        public HomeController(GamestoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var gamestoreContext = _context.Game.Include(g => g.Category);
+            return View(await gamestoreContext.ToListAsync());
+        }
+    }
+}
